293615cb664a00c03f431e41641586a7c8abe83d
Petr Hron <hronpet1@fit.cvut.cz>
2019-09-20 14:29:47 -0500

    Commited

--- DoesItBlend/Program.cs
+++ DoesItBlend/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DoesItBlend
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            List<IBlendable> blendables = new List<IBlendable>();
+            blendables.Add(new Strawberry());
+            blendables.Add(new Banana());
+            blendables.Add(new CellPhone());
+            blendables.Add(new IceCubes());
+            blendables.Add(new Banana());
+            
+            for (int i = 0; i < 10; i++)
+                blendables.Add(new Mango());
+
+                string mess = "";
+            foreach(IBlendable blendable in blendables){
+                mess += blendable.Blend();
+                mess += "\n";
+            }
+            Console.WriteLine(mess);
+
         }
     }
 }
